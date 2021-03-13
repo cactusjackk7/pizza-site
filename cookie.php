7b1a0859dcb7c129c853a2b948dc79f6b8e2712f
cactusjackk7 <cactusjackk7@gmail.com>
2021-03-13 13:08:16 +0100

    cookies for the site

---
+++ cookie.php
@@ -0,0 +1,14 @@
+<?php
+include 'index.html';
+
+require( __DIR__ . '/vendor/autoload.php' );
+
+$cookie = new \Overclokk\Cookie\Cookie( $_COOKIE );
+
+$cookie_name = 'cookie_name';
+
+$cookie_value = $cookie->get( $cookie_name );
+
+var_dump( $cookie_value === $_COOKIE[ $cookie_name ] );!
+
+?>
