b86f1e35d9562369f2150b2dd3de38181156f576
cactusjackk7 <cactusjackk7@gmail.com>
2021-03-17 12:19:59 +0100

    change it in a bit

--- cookie.php
+++ cookie.php
@@ -1,15 +1,5 @@
 <?php
 
-include 'index.html';
-
-require( __DIR__ . '/vendor/autoload.php' );
-
-$cookie = new \Overclokk\Cookie\Cookie( $_COOKIE );
-
-$cookie_name = 'cookie_name';
-
-$cookie_value = $cookie->get( $cookie_name );
-
-var_dump( $cookie_value === $_COOKIE[ $cookie_name ] );!
-
 ?>
+
+<!-- gonna change this -->
