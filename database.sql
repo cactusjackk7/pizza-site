f130105ed434ce37b3c2c839f80a9694e0be5731
cactusjackk7 <cactusjackk7@gmail.com>
2021-03-19 14:24:35 +0100

    added customer table

--- database.sql
+++ database.sql
@@ -1,1 +1,9 @@
 -- database voor soprano's --
+CREATE TABLE customer (
+   customer_id SERIAL ,
+   name VARCHAR ,
+   address VARCHAR ,
+   phone_num VARCHAR ,
+   payment_meth TEXT ,
+   PRIMARY KEY (customer_id)
+);
