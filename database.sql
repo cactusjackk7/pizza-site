9a64cc2099dede3288416084ef58c07aefdd876a
cactusjackk7 <cactusjackk7@gmail.com>
2021-03-19 20:54:43 +0100

    added FOREIGN KEY and is done

--- database.sql
+++ database.sql
@@ -88,3 +88,21 @@ CREATE TABLE user_login (
     salt VARCHAR ,
     PRIMARY KEY (customer_id)
 );
+
+ALTER TABLE card_join ADD FOREIGN KEY (customer_id) REFERENCES customer (customer_id);
+ALTER TABLE card_join ADD FOREIGN KEY (card_id) REFERENCES card (id);
+ALTER TABLE orders_join ADD FOREIGN KEY (customer_id) REFERENCES customer (customer_id);
+ALTER TABLE orders_join ADD FOREIGN KEY (orders_id) REFERENCES orders (id);
+ALTER TABLE pizza_data ADD FOREIGN KEY (pizza_data_id) REFERENCES pizza_data_join (pizza_data_id);
+ALTER TABLE pizza_data ADD FOREIGN KEY (pizza_data_id) REFERENCES ingredients_join (pizza_data_id);
+ALTER TABLE pizza_data_join ADD FOREIGN KEY (orders_id) REFERENCES orders (id);
+ALTER TABLE pizza_data_join ADD FOREIGN KEY (pizza_data_id) REFERENCES pizza_data (pizza_data_id);
+ALTER TABLE ingredients_join ADD FOREIGN KEY (ingredients_id) REFERENCES ingredients (id);
+ALTER TABLE ingredients_join ADD FOREIGN KEY (pizza_data_id) REFERENCES pizza_data (pizza_data_id);
+ALTER TABLE drinks_join ADD FOREIGN KEY (orders_id) REFERENCES orders (id);
+ALTER TABLE drinks_join ADD FOREIGN KEY (drink_id) REFERENCES drinks (id);
+ALTER TABLE preference ADD FOREIGN KEY (pizza_data_id) REFERENCES pizza_data (pizza_data_id);
+ALTER TABLE preference_join ADD FOREIGN KEY (preference_id) REFERENCES preference (id);
+ALTER TABLE preference_join ADD FOREIGN KEY (customer_id) REFERENCES customer (customer_id);
+ALTER TABLE user_login ADD FOREIGN KEY (customer_id) REFERENCES customer (customer_id);
+
