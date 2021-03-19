8fb7377feeb1e9169e0f5951b0e8599d182bebbc
cactusjackk7 <cactusjackk7@gmail.com>
2021-03-19 20:39:03 +0100

    checkpoints from files:

---
+++ .ipynb_checkpoints/database-checkpoint.sql
@@ -0,0 +1,71 @@
+-- database voor soprano's --
+CREATE TABLE customer (
+   customer_id SERIAL ,
+   name VARCHAR ,
+   address VARCHAR ,
+   phone_num VARCHAR ,
+   payment_meth TEXT ,
+   PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE card_join (
+   customer_id INTEGER , 
+   card_id INTEGER
+);
+
+CREATE TABLE orders_join (
+	customer_id  INTEGER ,
+	orders_id INTEGER
+
+);
+
+CREATE TABLE card (
+	id  SERIAL ,
+	card_num VARCHAR ,
+	PRIMARY KEY (id)
+
+);
+
+CREATE TABLE orders(
+	id  SERIAL ,
+	orders_date TIMESTAMP ,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE pizza_data (
+   pizza_data_id SERIAL ,
+   size VARCHAR ,
+   type VARCHAR ,
+   price DECIMAL ,
+   happy_hour BOOLEAN NOT NULL DEFAULT 'False' ,
+   PRIMARY KEY (pizza_data_id)
+);
+
+CREATE TABLE pizza_data_join (
+   pizza_data_id INTEGER ,
+   orders_id INTEGER
+);
+
+CREATE TABLE ingredients (
+  id SERIAL ,
+  name VARCHAR ,
+  price DECIMAL ,
+  PRIMARY KEY (id)  
+);
+
+CREATE TABLE ingredients_join (
+    pizza_data_id  INTEGER,
+    ingredients_id INTEGER
+);
+
+CREATE TABLE drinks (
+    id SERIAL ,
+    description VARCHAR ,
+    manufacturer VARCHAR ,
+    
+);
+
+CREATE TABLE drinks_join (
+    orders_id INTEGER ,
+    drink_id INTEGER
+);
